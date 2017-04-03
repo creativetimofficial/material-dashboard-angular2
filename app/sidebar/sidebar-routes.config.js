9059e0d35107fbc86727c7fd3f9b8d9a770c6896
chelaruc <chelaru_ciprian@yahoo.com>
2017-04-03 16:36:41 +0300

    Fixed burger menu

--- app/sidebar/sidebar-routes.config.js
+++ app/sidebar/sidebar-routes.config.js
@@ -7,5 +7,6 @@ exports.ROUTES = [
     { path: 'icons', title: 'Icons', icon: 'bubble_chart', class: '' },
     { path: 'maps', title: 'Maps', icon: 'location_on', class: '' },
     { path: 'notifications', title: 'Notifications', icon: 'notifications', class: '' },
+    { path: 'upgrade', title: 'Upgrade to PRO', icon: 'unarchive', class: 'active-pro' },
 ];
 //# sourceMappingURL=sidebar-routes.config.js.map
