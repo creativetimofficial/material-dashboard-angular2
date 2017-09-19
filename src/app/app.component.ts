d32079638c55f47ee0aa4643635eb00860fb7373
chelaruc <chelaru_ciprian@yahoo.com>
2017-09-19 13:04:13 +0300

    Fixed material initialisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 
+declare const $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +13,8 @@ export class AppComponent implements OnInit {
   constructor(public location: Location) {}
 
   ngOnInit() {
+      $.material.options.autofill = true;
+      $.material.init();
   }
 
     isMaps(path){
