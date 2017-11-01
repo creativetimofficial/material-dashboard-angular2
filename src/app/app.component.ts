ced40ae968130b48d3f5f83091a5bba13b9a5192
chelaruc <chelaru_ciprian@yahoo.com>
2017-11-01 12:09:44 +0200

    added changes for update - fixed IE - added PS - added scrolltop - changed angular-cli.json styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy, PopStateEvent } from '@angular/common';
+import 'rxjs/add/operator/filter';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import PerfectScrollbar from 'perfect-scrollbar';
 
 declare const $: any;
 
@@ -9,22 +14,69 @@ declare const $: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    private _router: Subscription;
+    private lastPoppedUrl: string;
+    private yScrollStack: number[] = [];
 
-  constructor(public location: Location) {}
+    @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
-  ngOnInit() {
-      $.material.options.autofill = true;
-      $.material.init();
-  }
+    constructor( public location: Location, private router: Router) {}
 
+    ngOnInit() {
+        $.material.init();
+        const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
+        const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+
+        this.location.subscribe((ev:PopStateEvent) => {
+            this.lastPoppedUrl = ev.url;
+        });
+         this.router.events.subscribe((event:any) => {
+            this.navbar.sidebarClose();
+            if (event instanceof NavigationStart) {
+               if (event.url != this.lastPoppedUrl)
+                   this.yScrollStack.push(window.scrollY);
+           } else if (event instanceof NavigationEnd) {
+               if (event.url == this.lastPoppedUrl) {
+                   this.lastPoppedUrl = undefined;
+                   window.scrollTo(0, this.yScrollStack.pop());
+               } else
+                   window.scrollTo(0, 0);
+           }
+        });
+        this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
+             elemMainPanel.scrollTop = 0;
+             elemSidebar.scrollTop = 0;
+        });
+        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+            let ps = new PerfectScrollbar(elemMainPanel);
+            ps = new PerfectScrollbar(elemSidebar);
+        }
+    }
+    ngAfterViewInit() {
+        this.runOnRouteChange();
+    }
     isMaps(path){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      titlee = titlee.slice( 1 );
-      if(path == titlee){
-        return false;
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+        titlee = titlee.slice( 1 );
+        if(path == titlee){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    runOnRouteChange(): void {
+      if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+        const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
+        const ps = new PerfectScrollbar(elemMainPanel);
+        ps.update();
       }
-      else {
-        return true;
-      }
+    }
+    isMac(): boolean {
+        let bool = false;
+        if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
+            bool = true;
+        }
+        return bool;
     }
 }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -21,7 +21,7 @@ const routes: Routes =[
     { path: 'maps',           component: MapsComponent },
     { path: 'notifications',  component: NotificationsComponent },
     { path: 'upgrade',        component: UpgradeComponent },
-      { path: '',          redirectTo: 'dashboard', pathMatch: 'full' }
+    { path: '',               redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
