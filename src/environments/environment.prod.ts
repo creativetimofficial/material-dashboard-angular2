aa223b93d18fae2034afd9562368103269347583
Trinhkien94 <kieict.94@gmail.com>
2019-08-31 02:56:52 +0700

    fix environment point to my server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authenticationUrl: 'https://trinhkien.herokuapp.com/authenticate',
-  articleUrl: 'https://trinhkien.herokuapp.com/articles'
+  authenticationUrl: 'https://149.28.142.76:8081/authenticate',
+  articleUrl: 'https://149.28.142.76:8081/articles'
 };
