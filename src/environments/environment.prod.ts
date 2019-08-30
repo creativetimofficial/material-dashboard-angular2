c14e7081e96859bc365b0620ba80ac5fb0136787
Trinhkien94 <kieict.94@gmail.com>
2019-08-31 02:59:48 +0700

    fix link production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authenticationUrl: 'https://149.28.142.76:8081/authenticate',
-  articleUrl: 'https://149.28.142.76:8081/articles'
+  authenticationUrl: '149.28.142.76:8081/authenticate',
+  articleUrl: '149.28.142.76:8081/articles'
 };
