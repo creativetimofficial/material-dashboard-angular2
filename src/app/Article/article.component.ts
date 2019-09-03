b7905799aa9aee85579276d93736b0f2b1a2fa9c
trinhkien <kieict.94@gmail.com>
2019-09-04 00:33:55 +0700

    fix page description and image

--- src/app/Article/article.component.ts
+++ src/app/Article/article.component.ts
@@ -3,6 +3,7 @@ import {ArticleDto} from '../dto/article-dto';
 import {HttpClientService} from '../service/httpclient.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Meta} from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article',
@@ -16,7 +17,7 @@ export class ArticleComponent implements OnInit {
   //
   // constructor(private httpClientService: HttpClientService, private route: ActivatedRoute, private router: Router) {
   // }
-  constructor(private httpClientService: HttpClientService, private route: ActivatedRoute, private router: Router, private meta: Meta) {
+  constructor(private titleService: Title, private httpClientService: HttpClientService, private route: ActivatedRoute, private router: Router, private meta: Meta) {
     route.params.subscribe(val => {
       const id = this.route.params['value'].id;
       this.httpClientService.getArticleById(id).subscribe(
@@ -38,10 +39,21 @@ export class ArticleComponent implements OnInit {
 
   handleSuccessfulResponse(response) {
     this.article = response;
-      const re = /<img/gi;
-      this.article.content = this.article.content.replace(re, '<img class="img-fluid"');
-      this.meta.updateTag({name: 'description', content: this.article.title});
-      this.meta.updateTag({property: 'og:image', content: this.article.image});
+    const re = /<img/gi;
+    this.article.content = this.article.content.replace(re, '<img class="img-fluid"');
+    this.setTitle(this.article.title);
+    this.createMetaTag();
   }
 
+  public setTitle( newTitle: string) {
+    this.titleService.setTitle( newTitle );
+  }
+  createMetaTag() {
+    // const meta = document.createElement('meta');
+    // meta.setAttribute('property','og:image'); //this line is the issue
+    // meta.content = '' + this.article.image;
+    // document.getElementsByTagName('head')[0].appendChild(meta);
+    this.meta.updateTag({name: 'description', content: this.article.title});
+    this.meta.updateTag({name: 'og:image', content: this.article.image});
+  }
 }
