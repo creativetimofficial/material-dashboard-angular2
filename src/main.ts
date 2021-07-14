615dbeebcd6060c592c4bc3b0d4a54c29ba06d68
rarestoma <tomairares@gmail.com>
2021-07-14 15:11:36 +0300

    Refactor: Change year

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/material-dashboard-angular2
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-angular2/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
