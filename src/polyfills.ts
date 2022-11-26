3e92953360c1d116391bc3541240445a336be471
taqishah1214 <60063724+taqishah1214@users.noreply.github.com>
2022-11-27 03:19:21 +0500

    angular 14 update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,32 +18,12 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 import 'web-animations-js';  // Run `npm install --save webå-animations-js`.
-
-
-/** Evergreen browsers require these. **/
-import 'core-js/es/reflect';
-// import 'core-js/es7/reflect';
-
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
 import 'web-animations-js';  // Run `npm install --save web-animations-js`.
