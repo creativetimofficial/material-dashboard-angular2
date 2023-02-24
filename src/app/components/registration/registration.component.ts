66916e58004cacfe62a78c6ef47f246bfe99708b
*rkeita31* <ramastone20@gmail.com>
2023-02-24 11:05:57 -0500

    formu okk

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Utilisateur } from 'app/model/utilisateur';
 import { UtilisateurService } from 'app/services/utilisateur.service';
+import * as bcrypt from 'bcryptjs'
 
 
 @Component({
@@ -10,24 +12,30 @@ import { UtilisateurService } from 'app/services/utilisateur.service';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-  user!:any[];
 
   utilisateur: Utilisateur = new Utilisateur();
+  myForm: FormGroup;
   constructor(private utilisateurService: UtilisateurService, private router: Router) { }
   ngOnInit(): void {
     this.saveUtilisateur();
+
     $('[name="btn1"]').click(function(){
       alert("Compte créé avec succés");
   })
   
+  
   }
 
   saveUtilisateur() {
-    this.utilisateurService.save(this.user).subscribe(
+    this.utilisateurService.save(this.utilisateur).subscribe(
       () => {
         this.utilisateur = new Utilisateur();
       }
     )
   }
-}
+  onSubmit() {
+    const password = this.myForm.get('password').value;
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
+}
+}
