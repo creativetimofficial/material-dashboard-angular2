181e72eb610e1da9273f509263fd115dabb38851
*rkeita31* <ramastone20@gmail.com>
2023-02-24 10:25:46 -0500

    formu okk

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -10,22 +10,20 @@ import { UtilisateurService } from 'app/services/utilisateur.service';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  user!:any[];
 
-
-
-
-  users!: any[]
-  roles!: any[];
   utilisateur: Utilisateur = new Utilisateur();
   constructor(private utilisateurService: UtilisateurService, private router: Router) { }
   ngOnInit(): void {
     this.saveUtilisateur();
-
+    $('[name="btn1"]').click(function(){
+      alert("Compte créé avec succés");
+  })
+  
   }
 
-
   saveUtilisateur() {
-    this.utilisateurService.save(this.utilisateur).subscribe(
+    this.utilisateurService.save(this.user).subscribe(
       () => {
         this.utilisateur = new Utilisateur();
       }
